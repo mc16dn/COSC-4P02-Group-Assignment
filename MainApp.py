61a86a78de669278a63adba48d307046c59eaf48
mc16dn <145287386+mc16dn@users.noreply.github.com>
2024-02-17 21:29:44 -0500

    Update MainApp.py
    Removed the testing

--- MainApp.py
+++ MainApp.py
@@ -73,32 +73,3 @@ def  censorText(postedstring):
         censored_post = ' '.join(words)
         
     return censored_post
-
-
-#TESTS
-print( "\n" + "Censored text: " + "\n")
-print(censorText(Post('https://www.reddit.com/r/stories/comments/1ahp9d1/meditation_practise_has_made_taking_shits_1000x/.json').text))
-
-#The following tests result in errors
-
-# The category doesn't work
-print(grab_url("AskReddit",10,"asdf", "asdf"))
-# The time period doesn't work
-print(grab_url("AskReddit",10,"top", "asdf"))
-# The count is too low
-print(grab_url("AskReddit",-10,"top", "month"))
-#This will succeed but will only output the 25 avaliable
-
-print(grab_url("AskReddit",10,"top", "month"))
-
- #The following tests succeed
-
-print(grab_url("AskReddit",0,"top", "month"))
-print(grab_url("AskReddit",100,"top", "month"))
-print(grab_url("AskReddit",20,"hot"))
-
-
-#Known issues
-#The maximum number of posts that can be retrieved is 25 but since that is in excess of a reasonable amount that may not be neccesary
-#The function inputs assumes that the subreddit exists
-#The function assumes that the input is the correct type
