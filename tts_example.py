666a373f966746332502aaaa1656145b01759d60
mc16dn <145287386+mc16dn@users.noreply.github.com>
2024-02-17 21:33:23 -0500

    Add files via upload
    Adds the tts software and an example of how to use it.

---
+++ tts_example.py
@@ -0,0 +1,13 @@
+from gtts import gTTS
+
+
+#lang is language, should always be en
+#tld is accent, us is american, com.au is Australian, and co.uk is British
+#for more options https://gtts.readthedocs.io/en/latest/module.html#localized-accents
+
+tts = gTTS("The quick brown fox jumps over the lazy dog", lang="en", tld='us')
+tts.save('first.mp3')
+tts = gTTS("The quick brown fox jumps over the lazy dog", lang="en", tld='com.au')
+tts.save('second.mp3')
+tts = gTTS("The quick brown fox jumps over the lazy dog", lang="en", tld='co.uk')
+tts.save('third.mp3')
