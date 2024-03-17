37fd2d66040aa4b43e06f78b79dbf63bfaa2e7fb
Matthew <mc16dn@brocku.ca>
2024-03-16 20:38:39 -0400

    Allowed the merging of audio and video as well as the creation of subtitles.

--- MainApp.py
+++ MainApp.py
@@ -1,9 +1,10 @@
 from ScraperClass import *
 from banned_words import *
-from urllib.request import urlopen
+from mutagen.mp3 import MP3
+from moviepy.editor import *
 import os
 import datetime
-import numpy
+import re
 from gtts import gTTS
 
 #Inputs subreddit name, the number of post taken, ordering used, and how far back posts go
@@ -75,16 +76,6 @@ def  censorText(postedstring):
 
     return censored_post
 
-
-
-#Creates a tts given a string of the text fro mthe posts and creates a mp3 file of it
-#Title: the title of the post
-def createTTS(postedstring,title, lan, accent):
-    tts = gTTS (postedstring, lang =lan, tld=accent)
-    tts.save(title+'.mp3')
-
-
-
 #Will return true if the duration is within the constraints and false if it is not. 
 #The constraints are set by the words_per_minute parameter and can be adjusted accordingly
 #maxTime: maximum amount of time of a video given in seconds. 
@@ -138,8 +129,74 @@ def textToSpeech(text, voice):
         return "Invalid entry to text to voice function"
 
 
-#Takes the text "I fucking love cats", cenosrs it, then creates a TTS audiofile that replaced the swear with REDACTED
-text = "I fucking love cats"
+#Merges the audio and video while also creating subtitles
+def merge(text, voice, video):
+
+    #Censoring the text then dividing it into pauses (anything that causes the speaker to pause)
+    #The subtitles and the audio are censored differently
+    #The word fuck will be censored as f*** in the text but as REDACTED in the audio
+    censortext = censorTTS(text)
+    words = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|\!|\,)\s',text)
+    censorwords = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|\!|\,)\s', censortext)
+    
+    #Keeping track of how long each sentence takes
+    total = 0
+    
+    #Merging the audio and video and then cutting it down to the audio's length to make future changes faster
+    clip = VideoFileClip(video).subclip(0, MP3("audio.mp3").info.length)
+    #The progress bar is hidden for this save so that the user will not believe it is done here
+    clip.write_videofile("temp.mp4", audio="audio.mp3", logger=None)
+    
+    #Creating the clips that make up individual subtitles then putting them in a list
+    clip = VideoFileClip("temp.mp4")
+    clips = [clip]
+    
+    #Determining how long a subtitle remains on screen
+    for x, word in enumerate(words):
+    
+        if voice == 1:
+            tts = gTTS(censorwords[x], lang="en", tld='us')
+            tts.save('sub.mp3')
+        
+        elif voice == 2:
+            tts = gTTS(censorwords[x], lang="en", tld='com.au')
+            tts.save('sub.mp3')
+
+        elif voice== 3:
+            tts = gTTS(censorwords[x], lang="en", tld='co.uk')
+            tts.save('sub.mp3')
+        
+        pre = total
+        total += MP3("sub.mp3").info.length
+        
+        #Accounting for any pauses caused by commas
+        if (words[x][-1] == ","):
+            total -= 0.192
+        
+        #Creating the subtitle
+        #This is where you make changes to edit the subtitles
+        #https://moviepy-tburrows13.readthedocs.io/en/improve-docs/ref/VideoClip/TextClip.html
+        sub = TextClip(words[x], fontsize = 90, color = 'black', size = (clip.w * 0.8,None), method = 'caption', bg_color = 'white')
+        sub = sub.set_start(pre)
+        sub = sub.set_pos('center').set_duration(total - pre)
+        clips.append(sub)
+
+    
+    #Adding the clips that have the subtitles
+    video = CompositeVideoClip(clips)
+    video.write_videofile("end.mp4")
+    
+    #Removing all temporary data
+    os.remove("temp.mp4")
+    os.remove("sub.mp3")
+    
+
+#Takes the text "I fucking love cats. I love dogs. I love all animals.", cenosrs it, then creates a TTS audiofile that replaced the swear with REDACTED
+
+text = "I fucking love cats. I love dogs. I love all animals."
 text = censorText(text)
 print(text)
+
+text = Post('https://www.reddit.com/r/stories/comments/1ahp9d1/meditation_practise_has_made_taking_shits_1000x/.json').text
 textToSpeech(text, 1)
+merge(text, 1, "vid1.mp4")
